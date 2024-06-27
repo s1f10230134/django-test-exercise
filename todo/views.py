7b25ed927cefddb3b1078ee4badc16527e9d07b5
1F10230134 <s1f102301347@iniad.org>
2024-06-27 20:58:23 +0900

    テストを追加

--- todo/views.py
+++ todo/views.py
@@ -8,7 +8,7 @@ def index(request):
     if request.method == 'POST':
         task = Task(title=request.POST['title'], 
                     due_at=make_aware(parse_datetime(request.POST['due_at'])))
-        task = save()
+        task.save()
 
     tasks = Task.objects.all()
 
